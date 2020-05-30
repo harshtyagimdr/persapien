1849f027d59100f1b68994d9a0feac5046a28fae
harshtyagimdr <harsh.17bcs1046@abes.ac.in>
2020-05-30 12:50:18 +0530

    Upload image

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as Path; 
+import 'package:firebase_storage/firebase_storage.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,7 +13,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
    File _image;    
   String _uploadedFileURL;
-
+  bool isLoading=false;
+  var value;
+  var id = DateTime.now().millisecondsSinceEpoch;
 
     Future chooseCamera() async {    
    await ImagePicker.pickImage(source: ImageSource.camera).then((image) {    
@@ -20,14 +24,27 @@ class _HomePageState extends State<HomePage> {
      });    
    });    
  } 
-
-  Future chooseFile() async {    
-   await ImagePicker.pickImage(source: ImageSource.gallery).then((image) {    
-     setState(() {    
-       _image = image ;    
+ Future uploadFile() async {   
+   isLoading=true;
+    
+    
+   StorageReference storageReference = FirebaseStorage.instance    
+       .ref()    
+       .child('images/$id/${Path.basename(_image.path)}}');    
+   StorageUploadTask uploadTask = storageReference.putFile(_image);    
+   await uploadTask.onComplete;    
+   print('File Uploaded');   
+ 
+   storageReference.getDownloadURL().then((fileURL) {    
+     setState(() {  
+      
+       _uploadedFileURL = fileURL; 
+       print(_uploadedFileURL);
+       isLoading=false;  
      });    
    });    
- } 
+ }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,23 +52,32 @@ class _HomePageState extends State<HomePage> {
     title: Text("Persapien"),
     centerTitle: true,
     ),
-    drawer: Drawer(),
+    
     floatingActionButton: FloatingActionButton(
-      onPressed: (){ _BottomSheet(context);},
+      onPressed: (){ chooseCamera();},
       child: Icon(Icons.camera_alt),
       ),
       body: Column(
         children: <Widget>[
           SizedBox(height: ScreenUtil.instance.setHeight(20),),
+
           Center(
             child:
             _image != null    
                ? Image.file(    
                    _image,    
-                   height: 150, 
+                   height:ScreenUtil.instance.setHeight(200), 
                     
                  )    
                : Container(child: Text("No Image Selected"),),
+          ),
+          SizedBox(height: ScreenUtil.instance.setHeight(20),),
+
+          _image==null || _uploadedFileURL!=null?Container():
+          RaisedButton(
+            child:Text('Upload',style:TextStyle(color:Colors.white)),
+            color:Colors.blue,
+            onPressed:uploadFile,
           )
         ],
       ),
@@ -59,29 +85,7 @@ class _HomePageState extends State<HomePage> {
 
   }
  
- void _BottomSheet(context){
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext bc){
-          return Container(
-            child: new Wrap(
-            children: <Widget>[
-        new ListTile(
-            leading: new Icon(Icons.camera),
-            title: new Text('Camera'),
-            onTap: chooseCamera,          
-          ),
-          new ListTile(
-            leading: new Icon(Icons.file_upload),
-            title: new Text('File'),
-            onTap:chooseFile,        
-          ),
-            ],
-          ),
-          );
-      }
-    );
-}
+
 
 }
 
